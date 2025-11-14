a7d8873e5bbaadc302a08c0ff2062b4a157568d8
leodev6 <djouakalionel4@gmail.com>
2025-11-14 19:23:45 +0100

    Implementazione della repository per le stastitiche

--- src/repositories/StatistiqueRepository.ts
+++ src/repositories/StatistiqueRepository.ts
@@ -7,17 +7,40 @@ export class StatatisticRepository implements IRepository<Statistic> {
           return await Statistic.create(entita as StatisticCreationAttributes);
      }
 
-     findById(id: number): Promise<Statistic | null> {
-          throw new Error('Method not implemented.');
+     async findById(id: number): Promise<Statistic | null> {
+          return await Statistic.findByPk(id);
      }
-     findAll(): Promise<Statistic[]> {
-          throw new Error('Method not implemented.');
+
+     async findAll(): Promise<Statistic[]> {
+          return await Statistic.findAll();
      }
-     update(id: number, entita: Partial<Statistic>): Promise<Statistic | null> {
-          throw new Error('Method not implemented.');
+
+     async update(id: number, entita: Partial<StatisticAttributes>): Promise<Statistic | null> {
+          const statistic = await Statistic.findByPk(id);
+          if (!statistic) {
+               return null;
+          }
+          await statistic.update(entita);
+          return statistic;
      }
-     delete(id: number): Promise<boolean> {
-          throw new Error('Method not implemented.');
+
+
+     async delete(id: number): Promise<boolean> {
+          const deleted = await Statistic.destroy({ where: { id }});
+          return deleted > 0;
+     }
+
+     //Metodi specifici per la Statistica
+     async findByType(type: string): Promise<Statistic[]>{
+          return await Statistic.findAll({ where: { type }});
+     }
+
+     async findByTypeAndDate(type: string, date: Date): Promise<Statistic | null> {
+          return await Statistic.findOne({
+               where: {
+                    type, date,
+               },
+          });
      }
 
 }
