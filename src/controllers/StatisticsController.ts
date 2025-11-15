9533ccfd82957decc04504968b9e8d6597c357fe
leodev6 <djouakalionel4@gmail.com>
2025-11-15 07:28:52 +0100

    Implementazione del controllore per il recupero delle tendenze giornaliere

--- src/controllers/StatisticsController.ts
+++ src/controllers/StatisticsController.ts
@@ -11,9 +11,9 @@ export class StatatisticsController {
           this.musicEventRepository = musicEventRepository;
      }
 
-     async getMostPlayedArtist(req: Request, res: Response): Promise<void>{
+     async getMostPlayedArtist(req: Request, res: Response): Promise<void> {
           try {
-               const date = req.query.date ? new Date(req.query.date as string):new Date();
+               const date = req.query.date ? new Date(req.query.date as string) : new Date();
                const events = await this.musicEventRepository.findByDate(date);
                const result = await this.statisticService.getMostPlayedArtist(events);
 
@@ -23,8 +23,28 @@ export class StatatisticsController {
                });
           } catch (error: any) {
                res.status(500).json({
-                    success:false,
+                    success: false,
                     error: error.message || 'Errore durant la ecuperazione dell\'artista più ascoltato',
+               });
+          }
+     }
+
+     async getDailyTrends(req: Request, res: Response): Promise<void> {
+          try {
+               const startDate = req.query.startDate ? new Date(req.query.startDate as string) : new Date(Date.now() - 7 * 24 * 60 * 1000); //Ultimi 7 giorni
+               const endDate = req.query.endDate ? new Date(req.query.endDate as string) : new Date();
+
+               const events = await this.musicEventRepository.findByDateRange(startDate, endDate);
+               const result = await this.statisticService.getDailyTrends(events);
+
+               res.json({
+                    success: true,
+                    data: result,
+               });
+          } catch (error: any) {
+               res.status(500).json({
+                    success: false,
+                    error: error.message || 'Errore durante il recupero delle tendenze giornaliere.'
                });
           }
      }
