f270b464dd01d50af30e3fd1c92008a200ea7dcd
leodev6 <djouakalionel4@gmail.com>
2025-11-15 07:34:33 +0100

    Implementazione del controllore il recupero della durate media

--- src/controllers/StatisticsController.ts
+++ src/controllers/StatisticsController.ts
@@ -29,6 +29,24 @@ export class StatatisticsController {
           }
      }
 
+     async getAverageDuration(req: Request, res: Response): Promise<void>{
+          try {
+               const date = req.query.date ? new Date(req.query.date as string) : new Date();
+               const events = await this.musicEventRepository.findByDate(date);
+               const result = await this.statisticService.getAverageDuration(events);
+
+               res.json({
+                    success: true,
+                    data: result
+               });
+          } catch (error: any) {
+               res.status(500).json({
+                    success: false,
+                    error: error.message || 'Errore durante il recupero della durate media.',
+               });
+          }
+     }
+
      async getDailyTrends(req: Request, res: Response): Promise<void> {
           try {
                const startDate = req.query.startDate ? new Date(req.query.startDate as string) : new Date(Date.now() - 7 * 24 * 60 * 1000); //Ultimi 7 giorni
